b5ac6ba765d2c0be8fe2529ee9a99631f7bf4b7f
Stas Kotelnykov <staskotelnykov@gmail.com>
2021-06-30 02:12:08 +0300

    commit-3

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -3,7 +3,7 @@ import FriendListItem from '../FriendListItem/FriendListItem';
 
 export default function FriendList({ friends }) {
   return (
-    <ul class="friend-list">
+    <ul className="friend-list">
       {friends.map(friend => (
         <li key={friend.id}>
           <FriendListItem
