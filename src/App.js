0137c420dd8ba7b9749bcc8bdfe2aa73b89412af
Stas Kotelnykov <staskotelnykov@gmail.com>
2021-06-30 01:55:00 +0300

    commit2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Profile from './components/Profile';
 import Statistics from './components/Statistics';
 import FriendList from './components/FriendList';
+import TransactionHistory from './components/TransactionHistory';
 import user from './user.json';
 import statisticalData from './statistical-data.json';
 import friends from './friends.json';
+import transactions from './transactions.json';
 
 export default function App() {
   return (
@@ -17,6 +19,7 @@ export default function App() {
       />
       <Statistics stats={statisticalData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
